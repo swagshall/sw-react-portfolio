d1d5eed901670d9f49a7c4207e7d0887e7b3a985
Shifra Wagshall <shifrawagshall@gmail.com>
2022-04-01 18:18:45 -0400

    Made some minor changes on the intro and started working on the header and footer.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import Intro from './components/Intro';
 import About from './components/About'
 import ProductList from './components/ProductList'
 import Contact from './components/Contact';
+// import Header from './components/common/Header'
+// import Footer from './components/common/Footer'
+
 
 // Inside our App component, we have a return method that contains all the JSX we want to render to the screen.
 // In this example, we have a parent `div` that references the Welcome component that we imported at the top.
 export default function App() {
   return (
     <div>
+      {/* <Header /> */}
       <Intro />
       <About />
       <ProductList />
       <Contact />
+      {/* <Footer /> */}
     </div>
   );
 }
